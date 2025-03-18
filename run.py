f1c00182edfb879c9a6083d9ec74847ba97be4c5
raphacarr <raphaelcarrilho7@gmail.com>
2025-03-18 09:40:14 +0100

    mise à jour run mode dev

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ Point d'entrée principal de l'application
 """
 from app.api.app import app
 from app.config.prod_config import PROD_API_HOST, PROD_API_PORT, PROD_API_DEBUG
+from app.config.dev_config import DEV_API_HOST, DEV_API_PORT, DEV_API_DEBUG
 
 if __name__ == '__main__':
-    # Lancement de l'API en mode production
-    print(f"Démarrage de l'API d'analyse de sentiments en mode PRODUCTION sur http://{PROD_API_HOST}:{PROD_API_PORT}")
-    app.run(host=PROD_API_HOST, port=PROD_API_PORT, debug=PROD_API_DEBUG)
+    # Lancement de l'API 
+    print(f"Démarrage de l'API d'analyse de sentiments en mode PRODUCTION sur http://{DEV_API_HOST}:{DEV_API_PORT}")
+    app.run(host=DEV_API_HOST, port=DEV_API_PORT, debug=DEV_API_DEBUG)
